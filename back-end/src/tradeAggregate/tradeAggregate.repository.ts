8f5cfe3baa449deffb26e544457822541888bf79
kkj6670 <48434120+kkj6670@users.noreply.github.com>
2021-12-17 09:53:24 +0900

    front-end, back-end repo분리

--- back-end/src/tradeAggregate/tradeAggregate.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { TradeAggregate } from './tradeAggregate.entity';
-
-@EntityRepository(TradeAggregate)
-export class TradeAggregateRepository extends Repository<TradeAggregate> {}
