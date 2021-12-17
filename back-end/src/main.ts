de31373a9dd2b050b216ff9f78ef50f2ec6726a7
kkj6670 <48434120+kkj6670@users.noreply.github.com>
2021-12-17 09:41:37 +0900

    chore: heroku 설정

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,9 +4,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(5000);
-
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(); /// cors허용
+  await app.listen(process.env.PORT || 5000);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
